71352c4d89a0760348dde5baa16a66422dd47354
markojaksic22 <ajdesamodaj@gmail.com>
2021-06-16 12:24:59 +0200

    package fix

--- src/components/Order.js
+++ src/components/Order.js
@@ -22,7 +22,7 @@ function Order({ id, amount, items, images }) {
             <div className='p-5 sm:p-10 '>
                 <div className='flex space-x-6 overflow-x-auto'>
                     {images.map(image => (
-                        <img src={image} alt="" className='h-20 object-contain s:h-32'/>
+                        <img src={image} alt="" className='h-20 object-contain sm:h-32'/>
                     ))}
                 </div>
             </div>
